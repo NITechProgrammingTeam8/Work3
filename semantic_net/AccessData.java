62f69c6fcf3abb8c724f79c313fcda7c9cfa9ae6
Hwater5 <shallowhotwater9.11@gmail.com>
2019-11-04 13:25:29 +0900

    searchDataの修正

--- semantic_net/AccessData.java
+++ semantic_net/AccessData.java
@@ -31,6 +31,7 @@ class AccessData {
 	public AccessData(SemanticNet sn) {
         this.sn = sn;
     }
+
     // GUI起動時にSemanticNetに対してテキストファイルの読み込みを指示する
     // 構築した関係のノードをArrayList<Link>で渡す(渡し方検討中)
     public void start(String filename) {
@@ -47,6 +48,7 @@ class AccessData {
     public ArrayList<Node> getNodes() {
     	return sn.getNodes();
     }
+
     // ファイルの読み込み
     private static List<String> readTextFile(String fileName) {
         List<String> stateList = new ArrayList<>();
@@ -64,6 +66,7 @@ class AccessData {
         }
         return stateList;
     }
+
     // 受け取った一行を空白で分割
     private static List<String> splitStatement(String statement) {
         return Arrays.asList(statement.split(" "));
@@ -83,5 +86,72 @@ class AccessData {
     /*
     // GUI終了時になにかしたいことがあれば…
     public void finish() {
+
+    }
+	*/
+    // 検索を行い結果を渡すように指示する
+    // 返すのはノード(ArrayList)？
+    public ArrayList searchData(String targetData) { // 複数の質問文は,区切りで与える
+    	// 検索文の分割
+    	List<String> querysplitList = splitStatementComma(targetData);
+    	ArrayList<Link> query = new ArrayList<Link>();
+    	for(String splitLists: querysplitList) {
+            List<String> splitList = splitStatement(splitLists);
+            addQuery(query, splitList);
+        }
+    	// 何を返してる？
+    	return sn.getQuery(query);
+    }
+
+    public ArrayList searchNaturalData(String targetData) {
+    	List<String> querysplitList = splitStatementComma(targetData);
+    	ArrayList<ArrayList<String>> queryList = new ArrayList<ArrayList<String>>();
+    	StringTokenizer st;		//トークンごとに分解
+    	for (String s : querysplitList) {
+    		ArrayList<String> tokenList = new ArrayList<>();
+    		st = new StringTokenizer(s);	//トークンごとに分解
+    		String firstToken = st.nextToken();
+    		String secondToken = st.nextToken();
+    		if(firstToken.equals("What")) {
+    			if(secondToken.equals("does")) {
+    				String thirdToken = st.nextToken();
+    				if(thirdToken.equals("the")) {
+    					thirdToken = st.nextToken();
+    				}
+    				tokenList.add(thirdToken);
+    				String forthToken = st.nextToken();
+    				if(forthToken.equals("have")) {
+    					tokenList.add("has-a");
+    				} else {
+    					tokenList.add(forthToken);
+    				}
+    				tokenList.add("?x");
+    			} else if(secondToken.equals("is")) {
+    				String thirdToken = st.nextToken().replace("'s", "");
+    				tokenList.add(thirdToken);
+    				tokenList.add(st.nextToken());
+    				tokenList.add("?x");
+    			}
+    		} else if(firstToken.equals("Is")) {
+    			tokenList.add(secondToken);
+    			tokenList.add(firstToken.replace("Is", "is-a"));
+    			st.nextToken();
+    			tokenList.add(st.nextToken());
+    		} else if(firstToken.equals("Does")) {
+    			tokenList.add(secondToken);
+    			tokenList.add(st.nextToken());
+    			String forthToken = st.nextToken();
+    			if(forthToken.equals("a")) {
+    				forthToken = st.nextToken();
+    			}
+    			tokenList.add(forthToken);
+    		}
+    		queryList.add(tokenList);
+    	}
+    	ArrayList<Link> query = new ArrayList<Link>();
+    	for(List<String> splitLists: queryList) {
+            addQuery(query, splitLists);
+        }
+    	return sn.getQuery(query);
     }
 }
